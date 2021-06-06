c2c2e34052127d41226e9fa86015517eeaa13333
WillDerthick <85421625+WillDerthick@users.noreply.github.com>
2021-06-05 21:40:59 -0400

    Add files via upload

---
+++ CashSavings.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import pickle
+
+CurrentBalance = pickle.load(open("Balance.dat", "rb"))
+
+print("How much would you like to deposit?")
+Deposit = int(input())
+
+Goal = 950
+TotalBalance = CurrentBalance + Deposit
+X100 = TotalBalance / Goal
+PercentageToGoal = X100 * 100
+
+print("Your new balance is " + str(TotalBalance) + ".")
+print("You are " + str(PercentageToGoal) + " percent of the way to your goal.")
+
+pickle.dump(TotalBalance, open("Balance.dat", "wb"))
+
+
+
+
+
+
